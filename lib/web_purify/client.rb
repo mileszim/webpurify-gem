a33945f4393c890c9d6f7a9abf75547de28072f3
Miles Zimmerman <miles@zserver.org>
2013-06-24 15:02:31 -0700

    Fixed a few bugs, started commenting

--- lib/web_purify/client.rb
+++ lib/web_purify/client.rb
@@ -2,14 +2,23 @@ require_relative 'methods/filters'
 require_relative 'methods/blacklist'
 require_relative 'methods/whitelist'
 
+# WebPurify::Client
+#
+# The WebPurify::Client class maintains state of the request parameters like api_key, endpoint, etc.,
+#   and provides easy methods for accessing WebPurify
 class WebPurify::Client
   include WebPurify::Filters
   include WebPurify::Blacklist
   include WebPurify::Whitelist
   
+  # Initialize the class
+  #
+  # @param options [String, Hash] Either an API key string, or a hash of options
   def initialize(options)
     if options.is_a? String
-      @api_key = options
+      @api_key    = options
+      @endpoint   = :us
+      @enterprise = false
     elsif options.is_a? Hash
       @api_key    = options[:api_key]
       @endpoint   = options[:endpoint]   || :us
--- lib/web_purify/request.rb
+++ lib/web_purify/request.rb
@@ -4,16 +4,33 @@ require 'net/https'
 require 'json'
 
 module WebPurify
+  
+  # WebPurify::Request
+  #
+  # WebPurify::Request handles the HTTP/HTTPS queries to the API endpoints,
+  #   converting returned JSON into a usable object.
   module Request
-
+    
     WRAPPER = :rsp
 
+
+    # Converts a hash of key/values into a url-ready query string
+    #
+    # @param hash [Hash] The hash to be converted
+    # @return [String] The formatted query string
     def self.to_query(hash)
       parameters = []
       hash.each {|k,v| parameters << "#{k}=#{v}" }
       return URI.encode(parameters.join("&"))
     end
-
+    
+    
+    # Executes a query to the API endpoint
+    #
+    # @param request_base [Hash] The base parameters for the request (comes from WebPurify::Client initialize())
+    # @param query_base   [Hash] The base parameters for the query (api_key, format)
+    # @param params       [Hash] The unique query parameters
+    # @return             [Hash] A hash parsed from the JSON response
     def self.query(request_base, query_base, params)
       q = query_base.merge(params)
       uri_builder = (request_base[:scheme]=='https') ? URI::HTTPS : URI::HTTP
@@ -25,9 +42,20 @@ module WebPurify
       return JSON.parse(WebPurify::Request.get(uri, request_base[:scheme]), :symbolize_names => true)[WRAPPER]
     end
 
+    
+    # Handles making the query according to http or https scheme
+    #
+    # @param uri    [String] The uri to be queried
+    # @param scheme [String] The scheme (http, https)
+    # @return       [String] The JSON request response
     def self.get(uri, scheme)
       req = (scheme=='https') ? Net::HTTPS : Net::HTTP
-      return req.get(uri)
+      begin
+        request = req.get(uri)
+      rescue Exception => e
+        p e
+      end
+      return request
     end
 
   end
