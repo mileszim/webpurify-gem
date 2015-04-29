66723511f8f316fecabdc205a63749bf55c1043b
jwhitis <jack.whitis@gmail.com>
2015-04-29 12:58:33 -0500

    Change locale back to endpoint to avoid confusion with language

--- lib/web_purify/client.rb
+++ lib/web_purify/client.rb
@@ -5,7 +5,7 @@ require_relative 'methods/image_filters'
 
 # WebPurify::Client
 #
-# The WebPurify::Client class maintains state of the request parameters like api_key, locale, etc.,
+# The WebPurify::Client class maintains state of the request parameters like api_key, endpoint, etc.,
 # and provides easy methods for accessing WebPurify
 class WebPurify::Client
   include WebPurify::TextFilters
@@ -19,16 +19,16 @@ class WebPurify::Client
   def initialize(options)
     if options.is_a? String
       @api_key    = options
-      @locale     = :us
+      @endpoint   = :us
       @enterprise = false
     elsif options.is_a? Hash
       @api_key    = options[:api_key]
-      @locale     = options[:locale]     || :us
+      @endpoint   = options[:endpoint]   || :us
       @enterprise = options[:enterprise] || false
     end
 
     @request_base = {
-      :host   => WebPurify::Constants.text_endpoints.fetch(@locale),
+      :host   => WebPurify::Constants.text_endpoints.fetch(@endpoint),
       :path   => WebPurify::Constants.rest_path,
       :scheme => WebPurify::Constants.scheme(@enterprise)
     }
